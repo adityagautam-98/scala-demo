2d29a46534ea9ad8b9e5e521c4c977c8f7eef3e0
Aditya Gautam <adigautam@deloitte.com>
2022-02-25 18:59:07 +0530

    Initial project setup.

---
+++ src/main/scala/movieAssignment.scala
@@ -0,0 +1,6 @@
+class movieAssignment(imdbTitle: String, title: String, originalTitle: String, year: Int, datePublished: String,
+                      genre: List[String], duration: Int, country: List[String], language: List[String],
+                      director: List[String], writer: List[String], productionCompany: String, actors: List[String],
+                      description: String, averageVote: Double, votes: Int, budget: String, usaGross: String,
+                      worldWideGross: String, metaScore: Int, userReviews: Int, criticsReviews: Int) {
+}
