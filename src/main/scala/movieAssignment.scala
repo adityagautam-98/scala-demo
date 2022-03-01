c33723c320fb492a78d5f89ac264408ac4a777c6
Aditya Gautam <adigautam@deloitte.com>
2022-03-01 18:29:32 +0530

    Milestone -1 completed, movies are filtered according to director name and given duration.

--- src/main/scala/movieAssignment.scala
+++ src/main/scala/movieAssignment.scala
@@ -4,7 +4,7 @@ case class Movie(imdb_title_id: String, title: String, original_title: String, y
                  genre: String, duration: Int, country: String, language: String,
                  director: String, writer: String, production_company: String, actors: String,
                  description: String, avg_vote: String, votes: String, budget: String, usa_gross_income: String,
-                 worlwide_gross_income: String, metascore: String, reviews_from_users: String, reviews_from_critics: String, _c22: String) {
+                 worlwide_gross_income: String, metascore: String, reviews_from_users: String, reviews_from_critics: String) {
 }
 
 object main extends App {
@@ -17,7 +17,7 @@ object main extends App {
     .csv("C:\\My-Projects\\Scala-Projects\\scala-demo\\src\\movies_dataset.csv") // removing header,and applying schema
   movieData.printSchema()
 
-  import sparkSession.implicits._
+  import sparkSession.implicits._// to resolve No implicits found for parameter evidence | line 24
 
   println(movieData.limit(100))
   val movieList = movieData.limit(10000)
@@ -25,6 +25,15 @@ object main extends App {
 
   println(movies)
 
+  def movieByDirectorForDuration(director:String, from:Int, till:Int): Unit = {
+    val startYear= (till-1).toString
+    val endYear=(from+1).toString
+    val selectedMovies = movies.filter(movies("director") === director &&
+      movies("year").lt(startYear) && movies("year").gt(endYear))
+    selectedMovies.show()
+  }
+
+  movieByDirectorForDuration("D.W. Griffith", 1910, 2020)
 
 
 }
