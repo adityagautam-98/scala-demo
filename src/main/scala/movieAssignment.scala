9b39ea92136bc7070c039e12a5d70225f130ef3e
Aditya Gautam <adigautam@deloitte.com>
2022-03-01 17:45:52 +0530

    Used SparkSession to read csv data from file.

--- src/main/scala/movieAssignment.scala
+++ src/main/scala/movieAssignment.scala
@@ -1,6 +1,25 @@
-class movieAssignment(imdbTitle: String, title: String, originalTitle: String, year: Int, datePublished: String,
+import org.apache.spark.sql.SparkSession
+case class movieAssignment(imdbTitle: String, title: String, originalTitle: String, year: Int, datePublished: String,
                       genre: List[String], duration: Int, country: List[String], language: List[String],
                       director: List[String], writer: List[String], productionCompany: String, actors: List[String],
                       description: String, averageVote: Double, votes: Int, budget: String, usaGross: String,
                       worldWideGross: String, metaScore: Int, userReviews: Int, criticsReviews: Int) {
 }
+
+object main extends App {
+  val sparkSession = SparkSession
+    .builder()
+    .appName("Spark SQL basic example").config("spark.master", "local")
+    .getOrCreate()
+
+  val movieData = sparkSession.read.option("header", "true")
+    .csv("C:\\My-Projects\\Scala-Projects\\scala-demo\\src\\movies_dataset.csv")// removing header,and applying schema
+
+
+  println(movieData.limit(100))
+  val movieList = movieData.limit(10000)
+
+}
+
+
+
