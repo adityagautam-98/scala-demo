757a1618dc40282fd8494cf3c0ca3ddf75c03f85
Aditya Gautam <adigautam@deloitte.com>
2022-03-01 23:20:25 +0530

    Milestone 2 completed, movies are filtered according to minimum user review and shown in descending order.

--- src/main/scala/movieAssignment.scala
+++ src/main/scala/movieAssignment.scala
@@ -40,82 +40,73 @@ object movieAssignment {
   }
 
   def buildMovieData(column: List[String]) = {
-    var year = 0;
-    var duration = 0;
-    var averageVote = 0
-    var votes = 0
-    var metaScore = 0
-    var userReviews = 0
-    var criticsReviews = 0
-
-    try {
-      year = column(3).toInt
-      duration = column(6).toInt
-      averageVote = column(14).toInt
-      votes = column(15).toInt
-      metaScore = column(19).toInt
-      userReviews = column(20).toInt
-      criticsReviews = column(21).toInt
-
-    }
-    catch {
-      case e: FileNotFoundException => println("The file can't be located. Please check the file path.")
-      case e: NumberFormatException => println("Unexpected datatype found, can't covert to required format.")
-
-    }
-    try {
-      movieData += Movie(column(0),
-        column(1),
-        column(2),
-        year,
-        column(4),
-        column(5).split(", ").toList,
-        duration,
-        column(7).split(", ").toList,
-        column(8).split(", ").toList,
-        column(9).split(", ").toList,
-        column(10).split(", ").toList,
-        column(11),
-        column(12).split(", ").toList,
-        column(13),
-        averageVote,
-        votes,
-        column(16),
-        column(17),
-        column(18),
-        metaScore,
-        userReviews,
-        criticsReviews)
-    }
-    catch {
-      case e: FileNotFoundException => println("Please check your file name and Location")
-      case e: NumberFormatException => println("Caught Number Format Exception: Cannot convert to needed format")
-    }
+    movieData += Movie(column(0),
+      column(1),
+      column(2),
+      if (column(3) == "" ) 0 else column(3).toInt,
+      column(4),
+      column(5).split(", ").toList,
+      if (column(6) == "" ) 0 else column(6).toInt,
+      column(7).split(", ").toList,
+      column(8).split(", ").toList,
+      column(9).split(", ").toList,
+      column(10).split(", ").toList,
+      column(11),
+      column(12).split(", ").toList,
+      column(13),
+      if (column(14) == "" ) 0 else column(14).toDouble,
+      if (column(15) == "" ) 0 else column(15).toInt,
+      column(16),
+      column(17),
+      column(18),
+      if (column(19) == "") 0 else column(19).toInt,
+      if (column(20) == "") 0 else column(20).toInt,
+      if (column(21) == "") 0 else column(21).toInt)
     movieData
   }
 
 
   def moviesByDirectorForDuration(director: String, fromYear: Int, tillYear: Int) = {
-    val result = movieData.filter(_.director.contains(director)).filter(_.year >= fromYear).filter(_.year <= tillYear)
-    println("Result for Query \n")
+    val selectedMovies = movieData.filter(_.director.contains(director)).filter(_.year >= fromYear).filter(_.year <= tillYear)
+    println("Result for Query on Directors and Year \n")
     println("IMDB NO. | Director |     Title     |     Original Title    | Year | Date Published | Genre | Duration | Country | Language | Writer | Production | Actors | Description")
-    for (i <- result) {
+    for (i <- selectedMovies) {
       println(s"${i.imdbTitle} | ${i.director} | ${i.title} | ${i.originalTitle} | ${i.year} | ${i.year} | ${i.datePublished} | ${i.genre} | ${i.duration} | ${i.country} | ${i.language} | ${i.writer} | ${i.productionCompany} | ${i.actor} | ${i.description}")
     }
-    result
+  }
+
+
+  def moviesWithUserReviews(minReview: Int) = {
+    val selectedMovies = movieData.filter(_.userReview > minReview)
+    val sortedList = selectedMovies.sortBy(_.userReview).reverse // to print in descending order
+    println("Result for filter on User Reviews \n")
+    println("S.No. | IMDB NO. | User Reviews |     Title    | Year | Date Published | Genre | Duration | Country | Language | Writer | Actors | Description")
+    var count = 0
+    for (i <- sortedList) {
+      count += 1
+      println(  s"${count}"+ s" | ${i.imdbTitle} | ${i.userReview} | ${i.title} | ${i.year} | ${i.year} | ${i.datePublished} | ${i.genre} | ${i.duration} | ${i.country} | ${i.language} | ${i.writer} | ${i.actor} | ${i.description}")
+    }
+
   }
 
 
   def main(args: Array[String]): Unit = {
     var count = 0;
-    val source = Source.fromFile("src/movies_dataset.csv")
-    source.getLines().drop(1)
-    while (source.hasNext && count < 10000) { // to only load 10000 records
-      val eachLine = readCSVLine(source)
-      buildMovieData(eachLine)
-      count += 1
+    try {
+      val source = Source.fromFile("src/movies_dataset.csv")
+      val lines = source.getLines().drop(1)
+      while (lines.hasNext && count < 10000) { // to only load 10000 records
+        val eachLine = readCSVLine(source)
+        buildMovieData(eachLine)
+        count += 1
+      }
+      moviesByDirectorForDuration("Francesco Bertolini", 1900, 2100)
+      moviesWithUserReviews(50)
     }
-    moviesByDirectorForDuration("Francesco Bertolini", 1900, 2100)
+    catch {
+      case e: FileNotFoundException => println("File not found, please verify the location.")
+      case e: NumberFormatException => println("Unexpected datatype found, can't convert to number.")
+    }
 
   }
 
