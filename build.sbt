9aaf54f17b6c84d969c0b32fe945dddc22c7a08a
Aditya Gautam <adigautam@deloitte.com>
2022-03-02 01:29:36 +0530

    Milestone 6 completed. Test Cases added for all methods.

--- build.sbt
+++ build.sbt
@@ -4,6 +4,5 @@ version := "0.1"
 
 scalaVersion := "2.13.8"
 libraryDependencies ++= Seq(
-
-
+  "org.scalatest" %% "scalatest" % "3.2.10" % "test",
 )
--- src/main/scala/movieAssignment.scala
+++ src/main/scala/movieAssignment.scala
@@ -11,7 +11,7 @@ object movieAssignment {
                    worldGross: String, metaScore: Int, userReview: Int, criticsReview: Int) {
   }
 
-  var movieData = ArrayBuffer[Movie]()
+
 
 
   def readCSVLine(source: Source): List[String] = {
@@ -39,7 +39,8 @@ object movieAssignment {
     ret.reverse.toList
   }
 
-  def buildMovieData(column: List[String]) = {
+  def buildMovieData( movieData: =>ArrayBuffer[Movie], column: List[String]) = { // call by name to retain movieData
+
     movieData += Movie(column(0),
       column(1),
       column(2),
@@ -66,31 +67,33 @@ object movieAssignment {
   }
 
 
-  def moviesByDirectorForDuration(director: String, fromYear: Int, tillYear: Int) = {
+  def moviesByDirectorForDuration( movieData:ArrayBuffer[Movie], director: String, fromYear: Int, tillYear: Int) = {
     val selectedMovies = movieData.filter(_.director.contains(director)).filter(_.year >= fromYear).filter(_.year <= tillYear)
     println("Result for Query on Directors and Year \n")
     println("IMDB NO. | Director |     Title     |     Original Title    | Year | Date Published | Genre | Duration | Country | Language | Writer | Production | Actors | Description")
     for (i <- selectedMovies) {
       println(s"${i.imdbTitle} | ${i.director} | ${i.title} | ${i.originalTitle} | ${i.year} | ${i.year} | ${i.datePublished} | ${i.genre} | ${i.duration} | ${i.country} | ${i.language} | ${i.writer} | ${i.productionCompany} | ${i.actor} | ${i.description}")
     }
+
+    selectedMovies
   }
 
 
-  def moviesWithUserReviews(minReview: Int) = {
-    val selectedMovies = movieData.filter(_.userReview > minReview)
-    val sortedList = selectedMovies.sortBy(_.userReview).reverse // to print in descending order
+  def moviesWithUserReviews( movieData:ArrayBuffer[Movie],minReview: Int) = {
+    val selectedMovies = movieData.filter(_.userReview > minReview).sortBy(_.userReview).reverse// to print in descending order
     println("Result for filter on User Reviews \n")
     println("S.No. | IMDB NO. | User Reviews |     Title    | Year | Date Published | Genre | Duration | Country | Language | Writer | Actors | Description")
     var count = 0
-    for (i <- sortedList) {
+    for (i <- selectedMovies) {
       count += 1
       println(s"${count}" + s" | ${i.imdbTitle} | ${i.userReview} | ${i.title} | ${i.year} | ${i.year} | ${i.datePublished} | ${i.genre} | ${i.duration} | ${i.country} | ${i.language} | ${i.writer} | ${i.actor} | ${i.description}")
     }
+    selectedMovies
 
   }
 
-  def longestDurationByCountryAndMinVotes(country: String, minimumVotes: Int) = {
-    val selectedMovies = movieData.filter(_.country.contains(country)).filter(_.vote >= minimumVotes).sortBy(_.duration).reverse.toArray
+  def longestDurationByCountryAndMinVotes( movieData:ArrayBuffer[Movie],country: String, minimumVotes: Int) = {
+    val selectedMovies = movieData.filter(_.country.contains(country)).filter(_.vote >= minimumVotes).sortBy(_.duration).reverse
     println("Result for filter on County and minimum votes \n")
     println("S.No. | IMDB NO. | Country | Duration  |  User Reviews  |     Title    | Year | Date Published | Genre | Language | Writer | Actors | Description")
     var count = 0
@@ -99,21 +102,24 @@ object movieAssignment {
       println(s"${count}" + s" | ${i.imdbTitle} | ${i.country} | ${i.duration} | ${i.userReview} | ${i.title} | ${i.year} | ${i.year} | ${i.datePublished} | ${i.genre} | ${i.language} | ${i.writer} | ${i.actor} | ${i.description}")
 
     }
+    println(selectedMovies)
+    selectedMovies
   }
 
   def main(args: Array[String]): Unit = {
+    val movieData = ArrayBuffer[Movie]()
     var count = 0;
     try {
       val source = Source.fromFile("src/movies_dataset.csv")
       val lines = source.getLines().drop(1)
       while (lines.hasNext && count < 10000) { // to only load 10000 records
         val eachLine = readCSVLine(source)
-        buildMovieData(eachLine)
+        buildMovieData(movieData, eachLine)
         count += 1
       }
-      moviesByDirectorForDuration("Francesco Bertolini", 1900, 2100)
-      moviesWithUserReviews(50)
-      longestDurationByCountryAndMinVotes("Australia", 10)
+      moviesByDirectorForDuration(movieData,"Francesco Bertolini", 1900, 2100)
+      moviesWithUserReviews(movieData,50)
+      longestDurationByCountryAndMinVotes(movieData,"Australia", 10)
     }
     catch {
       case e: FileNotFoundException => println("File not found, please verify the location.")
