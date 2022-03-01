d169c7f2fda3afc7edc5bdb9e7b5f627386e42bd
Aditya Gautam <adigautam@deloitte.com>
2022-03-01 18:17:57 +0530

    Converted csv data to Movie Dataset.

--- src/main/scala/movieAssignment.scala
+++ src/main/scala/movieAssignment.scala
@@ -1,9 +1,10 @@
-import org.apache.spark.sql.SparkSession
-case class movieAssignment(imdbTitle: String, title: String, originalTitle: String, year: Int, datePublished: String,
-                      genre: List[String], duration: Int, country: List[String], language: List[String],
-                      director: List[String], writer: List[String], productionCompany: String, actors: List[String],
-                      description: String, averageVote: Double, votes: Int, budget: String, usaGross: String,
-                      worldWideGross: String, metaScore: Int, userReviews: Int, criticsReviews: Int) {
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+case class Movie(imdb_title_id: String, title: String, original_title: String, year: Int, date_published: String,
+                 genre: String, duration: Int, country: String, language: String,
+                 director: String, writer: String, production_company: String, actors: String,
+                 description: String, avg_vote: String, votes: String, budget: String, usa_gross_income: String,
+                 worlwide_gross_income: String, metascore: String, reviews_from_users: String, reviews_from_critics: String, _c22: String) {
 }
 
 object main extends App {
@@ -12,14 +13,24 @@ object main extends App {
     .appName("Spark SQL basic example").config("spark.master", "local")
     .getOrCreate()
 
-  val movieData = sparkSession.read.option("header", "true")
-    .csv("C:\\My-Projects\\Scala-Projects\\scala-demo\\src\\movies_dataset.csv")// removing header,and applying schema
+  val movieData = sparkSession.read.option("header", "true").option("inferSchema", true)
+    .csv("C:\\My-Projects\\Scala-Projects\\scala-demo\\src\\movies_dataset.csv") // removing header,and applying schema
+  movieData.printSchema()
 
+  import sparkSession.implicits._
 
   println(movieData.limit(100))
   val movieList = movieData.limit(10000)
+  val movies: Dataset[Movie] = movieList.as[Movie]
+
+  println(movies)
+
+
 
 }
+
+
+
 
 
 
